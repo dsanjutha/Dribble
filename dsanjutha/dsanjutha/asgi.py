c9b3f94d2067a677aecd4001a8a2b5f018683d85
dsanjutha <dsanjutha2007@gmail.com>
2025-10-15 08:52:04 +0530

    Intial commit

---
+++ dsanjutha/dsanjutha/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dsanjutha project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsanjutha.settings')
+
+application = get_asgi_application()
